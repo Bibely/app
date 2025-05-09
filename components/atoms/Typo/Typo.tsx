9afa6779a20082043a9e80d406803c24ffdaea12
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-05-09 16:56:37 +0900

    feat: update typography constants and enhance Typo component for better text styling

--- components/atoms/Typo/Typo.tsx
+++ components/atoms/Typo/Typo.tsx
@@ -1,20 +1,20 @@
 import { useTheme } from "@/hooks/useTheme";
-import { TypoProps, TypoWeight } from "./Typo.type";
-
+import { TypoProps } from "./Typo.type";
 import { Text, TextStyle } from "react-native";
 import { Color } from "@/constants/token/color";
+import { Typography } from "@/constants/token/typography";
 
-export default function Typo(props: TypoProps) {
+function TypoBase(props: TypoProps) {
   const theme = useTheme();
   const {
     children,
     style,
-    color,
+    color = "text",
     textAlign,
     numberOfLines,
     ellipsizeMode,
-    size = 16,
-    weight = TypoWeight.Medium,
+    size = Typography.fontSize.body,
+    weight = Typography.fontWeight.medium,
     maxWidth,
     testID,
   } = props;
@@ -23,8 +23,8 @@ export default function Typo(props: TypoProps) {
     width: maxWidth ? "100%" : undefined,
     textAlign,
     fontSize: size,
-    fontWeight: weight,
-    color: Color[theme][color ?? "text"],
+    fontWeight: weight.toString() as TextStyle["fontWeight"],
+    color: Color[theme][color],
     ...style,
   };
 
@@ -39,3 +39,34 @@ export default function Typo(props: TypoProps) {
     </Text>
   );
 }
+
+function createTypoFromToken(
+  sizeKey: keyof typeof Typography.fontSize,
+  weightKey: keyof typeof Typography.fontWeight
+) {
+  return (props: Omit<TypoProps, "size" | "weight">) => (
+    <TypoBase
+      {...props}
+      size={Typography.fontSize[sizeKey]}
+      weight={Typography.fontWeight[weightKey]}
+      style={{
+        lineHeight: Typography.lineHeight[sizeKey],
+        ...(props.style || {})
+      }}
+    />
+  );
+}
+
+export const Typo = Object.assign(TypoBase, {
+  Caption: createTypoFromToken("caption", "medium"),
+  Footnote: createTypoFromToken("caption", "medium"),
+  Body: createTypoFromToken("body", "medium"),
+  Subtitle: createTypoFromToken("subtitle", "medium"),
+  Title: createTypoFromToken("title", "bold"),
+  Heading: createTypoFromToken("heading", "bold"),
+  DisplaySmall: createTypoFromToken("displaySmall", "bold"),
+  DisplayMedium: createTypoFromToken("displayMedium", "bold"),
+  DisplayLarge: createTypoFromToken("displayLarge", "bold"),
+});
+
+export default Typo;
--- components/atoms/Typo/Typo.type.ts
+++ components/atoms/Typo/Typo.type.ts
@@ -13,8 +13,8 @@ export interface TypoStyleProps {
     style?: TextStyle;
     
     textAlign?: 'auto' | 'left' | 'right' | 'center' | 'justify'; // 추가: 텍스트 정렬
-	numberOfLines?: number; // 추가: 텍스트 표시 줄 수
-	ellipsizeMode?: 'head' | 'middle' | 'tail' | 'clip'; // 추가: 텍스트가 넘칠 때 처리 방식
+      numberOfLines?: number; // 추가: 텍스트 표시 줄 수
+      ellipsizeMode?: 'head' | 'middle' | 'tail' | 'clip'; // 추가: 텍스트가 넘칠 때 처리 방식
 
     maxWidth?: boolean; // 추가: 텍스트 최대 너비
 
