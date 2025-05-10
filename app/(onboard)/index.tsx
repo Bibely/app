9d1c6d898c327a27238c0d1f5e43e10c19d53e69
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-05-10 21:24:55 +0900

    feat: implement localization support and update layout with gradient background

--- app/(onboard)/index.tsx
+++ app/(onboard)/index.tsx
@@ -1,31 +1,15 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { Image, Platform, StyleSheet } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
-import Logo from '@/assets/images/icon.png';
-import { HStack, Typo, VStack } from '@/components/atoms';
+import { Typo, VStack } from '@/components/atoms';
 import { Icon } from '@/components/icon/glyph';
 import SocialLogin from '@/components/molecules/SocialLogin/SocialLogin';
 import { SocialLoginPlatform } from '@/components/molecules/SocialLogin/SocialLogin.type';
-import { Color, Radius, Spacing } from '@/constants/token';
+import { Color, Spacing } from '@/constants/token';
 import { useTheme } from '@/hooks/useTheme';
 
 export default function Onboard() {
-  const inset = useSafeAreaInsets();
   const theme = useTheme();
 
   return (
-    <LinearGradient
-      colors={['#fff', '#fff', 'rgb(255, 235, 231)']}
-      style={{
-        ...s.container,
-        paddingTop: Platform.OS === 'android' ? inset.top + Spacing.medium : inset.top,
-        paddingBottom: Platform.OS === 'android' ? inset.bottom + Spacing.big : inset.bottom,
-      }}
-    >
-      <HStack justify={'center'}>
-        <Image source={Logo} style={s.logo} />
-      </HStack>
+    <>
       <VStack bgColor={'transparent'} gap={Spacing.large}>
         <Icon.quote size={52} color={Color[theme].brand10} />
         <Typo size={30} readingMode>
@@ -47,22 +31,6 @@ export default function Onboard() {
           그리고 개인정보 취급 처리방침
         </Typo>
       </VStack>
-    </LinearGradient>
+    </>
   );
 }
-
-const s = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
-    paddingInline: Spacing.big,
-  },
-  logo: {
-    width: 50,
-    height: 50,
-    borderRadius: Radius.medium,
-    marginTop: Spacing.medium,
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,22 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import 'react-native-reanimated';
 
-import { useTheme } from "@/hooks/useTheme";
-import { Color } from "@/constants/token/color";
-import "react-native-reanimated";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+
+import { useTheme } from '@/hooks/useTheme';
+import { Color } from '@/constants/token';
+
+import '@/utils/locale/il8n';
 
 const CustomDefaultTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: Color["light"]["surface"],
+    background: Color['light']['surface'],
   },
 };
 
@@ -25,7 +24,7 @@ const CustomDarkTheme = {
   ...DarkTheme,
   colors: {
     ...DarkTheme.colors,
-    background: Color["dark"]["surface"],
+    background: Color['dark']['surface'],
   },
 };
 
@@ -35,10 +34,10 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const theme = useTheme();
   const [loaded, error] = useFonts({
-    PretendardMedium: require("../assets/fonts/Pretendard-Medium.otf"),
-    PretendardSemibold: require("../assets/fonts/Pretendard-SemiBold.otf"),
-    PretendardBold: require("../assets/fonts/Pretendard-Bold.otf"),
-    BookkMyungjo: require("../assets/fonts/BookkMyungjo.ttf"),
+    PretendardMedium: require('../assets/fonts/Pretendard-Medium.otf'),
+    PretendardSemibold: require('../assets/fonts/Pretendard-SemiBold.otf'),
+    PretendardBold: require('../assets/fonts/Pretendard-Bold.otf'),
+    BookkMyungjo: require('../assets/fonts/BookkMyungjo.ttf'),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -58,9 +57,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider
-      value={theme === "dark" ? CustomDarkTheme : CustomDefaultTheme}
-    >
+    <ThemeProvider value={theme === 'dark' ? CustomDarkTheme : CustomDefaultTheme}>
       <Stack initialRouteName="(onboard)">
         {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
